f4825fe9353fcd89e031652370fb3de024d08006
Matteo Affatato <matteoaffatato@MacBook-Air-di-Matteo.local>
2023-12-13 19:38:24 +0100

    primo commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// ES 1 :
+// Realizzare un oggetto che rappresenti un garage, dovrà contenere una lista di automobili. Ciascuna automobile dovra’ avere una marca (es. Fiat) ed una lista di modelli (es. Pandino tattico rosso).
+// Scrivere un metodo che prenda in input una marca e stampi i modelli presenti nel garage di quella stessa marca.
+
+
+// let garage = {
+//     'auto' : [
+//     {'marca':'tesla', 'model': ['modelS','modelX','modelY']},
+//     {'marca':'jeep', 'model': ['renegade','compass','wrangler']},
+//     {'marca':'bmw', 'model': ['serie1', 'serie3', 'serie5']},
+//     ],
+   
+//     'inputBrand': function(brand){
+//         this.auto.forEach((el) => {
+//         if(brand.toLowerCase() == el.marca.toLowerCase()){
+//             console.log(el.model);
+//         }
+//     })        
+//     return'Non abbiamo questa auto in garage';
+// } 
+// }
+
+// garage.inputBrand('tesla');
+
+
+
+
+
+
+
+
+
+
+
